54b719d67732f5ccde106c7d66c3b4f3e855b1e7
Isuru Wimalasiri <isuru.wimalasiri1997@gmail.com>
2023-05-09 00:25:03 +0530

    creating the app layout and landing page with tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from 'next/image';
+import HeroImage from '../public/hero.webp';
+import LogoBanner from '../components/LogoBanner/LogoBanner';
 
 export default function Home() {
-  const { user } = useUser();
   return (
-    <div>
-      {user ? (
-        <>
-          <div>
-            <Image src={user.picture} alt={user.name} height={50} width={50} />
-            <div>{user.email}</div>
-          </div>
-          <Link href="/api/auth/logout">Logout</Link>
-        </>
-      ) : (
-        <Link href="/api/auth/login">Login</Link>
-      )}
+    <div className="w-screen h-screen flex items-center justify-center relative">
+      <Image src={HeroImage} alt="Hero" fill className="absolute" />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
+        <LogoBanner />
+        <p>
+          The AI-powered SAAS solution to generate SEO-optimized blog posts in
+          minutes. Get high-quality content, without sacrificing your time.
+        </p>
+        <Link href="/post/new" className="btn">
+          BEGIN
+        </Link>
+      </div>
     </div>
   );
 }
